c29fbb259dd6c481b161e47f3f8b81475bbb953b
brskrt <gevendee@gmail.com>
2025-02-03 16:57:31 +0300

    Update author name to Barış Kurt

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -53,7 +53,7 @@ export default async function Page({ params }: Props) {
                 <svg className="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
                 </svg>
-                <span className="font-medium">{post.author}</span>
+                <span className="font-medium">Barış Kurt</span>
               </div>
               
               <div className="flex items-center text-emerald-700">
